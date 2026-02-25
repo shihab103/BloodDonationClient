7595fbb12af72f7395108e3779e35d4abe5db78b
Shihab Uddin <shihabuddin2469@gmail.com>
2026-02-25 22:57:22 +0600

    added utils folder

---
+++ src/utils/axiosSecure.js
@@ -0,0 +1,20 @@
+// utils/useAxiosSecure.js
+import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthContext";
+
+export const useAxiosSecure = () => {
+  const { user } = useContext(AuthContext);
+
+  const instance = axios.create({
+    // baseURL: "https://server-tawny-mu.vercel.app/",
+    baseURL: "http://localhost:5000",
+    headers: user?.accessToken
+      ? {
+          Authorization: `Bearer ${user.accessToken}`,
+        }
+      : {},
+  });
+
+  return instance;
+};
